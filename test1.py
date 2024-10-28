39eb3fe783bd66c6f6de5659d808bb154cba73d0
aathira-writes <162028853+aathira-writes@users.noreply.github.com>
2024-10-28 21:03:45 +0000

    test2

--- test1.py
+++ test1.py
@@ -1,7 +1,8 @@
 import numpy as np
-import pandas as pan
+import pandas as pd
 import matplotlib.pyplot as pyplot
 import seaborn as sns
 sns.set(color_codes = True)
 %matplotlib inline
-
+foresthealthdata = pd.read_csv('forest_health_data.csv')
+foresthealthdata.count()
